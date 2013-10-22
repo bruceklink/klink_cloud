3b449fb27e3e5283bca58808b631f92a6d73730c
Bruce M. Klink <b-klink@kellogg.northwestern.edu>
2013-10-22 01:44:15 -0500

    files from rails new command

---
+++ klink_cloud/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KlinkCloud::Application.config.secret_key_base = '95f69d1bb50a5ec0643bd510a659488c7915731a0ba2fe722b65cdfcd6cfdfff6be496f2d3306650a33d0f27975431f98c369e2616c91f956a8fca5c184ee0cc'
